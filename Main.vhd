cbe7481a986039c9214bc30dd1d5a49b1b29a49b
Allan Garcia <allan.garcia@wr.sc.usp.br>
2023-06-15 17:13:39 -0300

    pc clk on btn

--- Main.vhd
+++ Main.vhd
@@ -10,6 +10,7 @@ entity Main is
     );
     port (
         clk_50mhz        : in std_logic;
+        clk_btn          : in std_logic;
         vga_h_sync       : out std_logic;
         vga_v_sync       : out std_logic;
         vga_r            : out std_logic_vector(3 downto 0);
@@ -44,7 +45,7 @@ architecture rtl of Main is
 
 begin
 
-    pc_clk           <= clk_50mhz;
+    pc_clk           <= clk_btn;
     ram_addr_final   <= std_logic_vector(unsigned(proc_out)/4 + unsigned(ram_addr));
     vga_pixel_clk    <= output_pixel_clk and char_clk;
     processor_output <= proc_out;
