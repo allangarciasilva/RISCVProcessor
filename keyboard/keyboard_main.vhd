a92a60f1d33d0795f3c57aa04a854d8b9fc1431f
Allan Garcia <allan.garcia@wr.sc.usp.br>
2023-06-24 16:01:47 -0300

    test kb

--- keyboard/keyboard_main.vhd
+++ keyboard/keyboard_main.vhd
@@ -15,26 +15,59 @@ end entity;
 
 architecture rtl of keyboard_main is
 
-    signal reg_clk : std_logic;
-    signal value   : std_logic_vector(n_displays * 4 - 1 downto 0);
+    -- signal reg_clk : std_logic;
+    -- signal value   : std_logic_vector(n_displays * 4 - 1 downto 0);
+
+    signal shift_register : std_logic_vector(21 downto 0) := (others => '0');
+    signal shift_counter  : integer range 0 to 11         := 0;
+
+    signal display_data : std_logic_vector(n_displays * 4 - 1 downto 0);
 
 begin
 
-    reg_clk <= not ps2_clk;
+    process (ps2_clk)
+    begin
+
+        if falling_edge(ps2_clk) then
+
+            shift_register(shift_register'length - 1) <= ps2_data;
+
+            for i in 1 to shift_register'length - 1 loop
+                shift_register(i - 1) <= shift_register(i);
+            end loop;
+
+            if shift_counter = 10 then
+                shift_counter <= 0;
+            else
+                shift_counter <= shift_counter + 1;
+            end if;
+
+        end if;
+
+    end process;
 
     arr : entity work.SevenSegmentsArray
         generic map(
             n_displays => n_displays)
         port map(
-            value => value,
+            value => display_data,
             leds  => leds);
 
-    reg : entity work.ShiftRegister
-        generic map(
-            width => value'length)
-        port map(
-            clk => reg_clk,
-            d   => ps2_data,
-            q   => value);
+    display_data(7 downto 0) <= shift_register(8 downto 1) when shift_counter = 0 else
+    (others => '0');
+
+    display_data(15 downto 8) <= shift_register(19 downto 12) when shift_counter = 0 else
+    (others => '0');
+
+    display_data(n_displays * 4 - 1 downto 16) <= (others => '0');
+
+    -- reg_clk <= not ps2_clk;
+    -- reg : entity work.ShiftRegister
+    --     generic map(
+    --         width => value'length)
+    --     port map(
+    --         clk => reg_clk,
+    --         d   => ps2_data,
+    --         q   => value);
 
 end architecture;
